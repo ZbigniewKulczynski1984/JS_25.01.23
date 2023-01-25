8402c0e18d82158b686bede1c1c2b902b20026ac
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-01-25 10:33:57 +0100

    code added

--- app.js
+++ app.js
@@ -21,13 +21,33 @@ checkAge('Goga', 27);
 checkAge('Zbig', 38);
 checkAge('Karol', 37);
 
-const veggies = ["celery", 'broccoli', 'cauliflower', 'carrots', 'green beans']
+const veggies = ['celery', 'broccoli', 'cauliflower', 'carrots', 'green beans'];
 
-for(i=0; i < veggies.length; i++){
-    console.log(veggies[i]);
+for (i = 0; i < veggies.length; i++) {
+	console.log(veggies[i]);
 }
 
 const users = [
-    {name: "Will", age: 23}, {name: "Ben", age: 22}, {name: "Alex", age: 20}, 
-    {name: "Gabi", age: 2}, {name: "Elisa", age: 1}   
+	{ name: 'Will', age: 23 },
+	{ name: 'Ben', age: 22 },
+	{ name: 'Alex', age: 20 },
+	{ name: 'Gabi', age: 2 },
+	{ name: 'Elisa', age: 1 },
 ];
+
+console.log(users);
+for (i = 0; i < users.length; i++) {
+	checkAge(users[i].name, users[i].age);
+}
+
+function getLength(string) {
+	length = string.length;
+	return length;
+}
+var length;
+getLength('Hello World');
+if (length % 2) {
+	console.log('The world is nice and even!');
+} else {
+	console.log('The world is an odd place!');
+}
