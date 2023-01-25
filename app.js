4f842f0ef65a3a3aad96a1d745dc725861fa5fea
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-01-25 10:33:04 +0100

    code added

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function sayHello() {
 
 sayHello();
 
-function checkage(name, age) {
+function checkAge(name, age) {
 	if (age < 21) {
 		console.log(`Sorry ${name}, you aren't old enough to view this page!!!`);
 	}
@@ -20,3 +20,14 @@ checkAge('Aga', 21);
 checkAge('Goga', 27);
 checkAge('Zbig', 38);
 checkAge('Karol', 37);
+
+const veggies = ["celery", 'broccoli', 'cauliflower', 'carrots', 'green beans']
+
+for(i=0; i < veggies.length; i++){
+    console.log(veggies[i]);
+}
+
+const users = [
+    {name: "Will", age: 23}, {name: "Ben", age: 22}, {name: "Alex", age: 20}, 
+    {name: "Gabi", age: 2}, {name: "Elisa", age: 1}   
+];
