6e7a71007e2c6f8c15315163b8e014a30c54c81e
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-01-25 10:31:13 +0100

    code added

--- app.js
+++ app.js
@@ -5,7 +5,18 @@ let states = 50;
 const nine = 5 + 4;
 
 function sayHello() {
-    console.log('Hello World')
+	console.log('Hello World');
 }
 
 sayHello();
+
+function checkage(name, age) {
+	if (age < 21) {
+		console.log(`Sorry ${name}, you aren't old enough to view this page!!!`);
+	}
+}
+
+checkAge('Aga', 21);
+checkAge('Goga', 27);
+checkAge('Zbig', 38);
+checkAge('Karol', 37);
