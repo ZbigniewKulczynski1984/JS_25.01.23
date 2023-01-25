296535bccd7858fe4d8b90386f158584d3739441
Zbigniew Kulczński <z-kulczynski@o2.pl>
2023-01-25 10:24:44 +0100

    code added

---
+++ app.js
@@ -0,0 +1,11 @@
+let name = 'Aga';
+let states = 50;
+
+// variable nine is assigned the addition of 5 and 4
+const nine = 5 + 4;
+
+function sayHello() {
+    console.log('Hello World')
+}
+
+sayHello();
